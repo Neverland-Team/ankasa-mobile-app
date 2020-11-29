94bd247aa341b7b613e295fba36350e2f04903ca
dyaksa <diasnour0395@gmail.com>
2020-11-29 18:32:16 +0700

    fixed flight

--- src/Users/Models/flightModel.js
+++ src/Users/Models/flightModel.js
@@ -14,42 +14,36 @@ module.exports = {
     });
   },
 
-  // getId: (id) => {
-  //   return new Promise((resolve, reject) => {
-  //     db.query(`SELECT * FROM flight WHERE idflight='${id}'`, (err, result) => {
-  //       if (err) {
-  //         reject(new Error(err));
-  //       } else {
-  //         resolve(result);
-  //       }
-  //     });
-  //   });
-  // },
+  getId: (id) => {
+    return new Promise((resolve, reject) => {
+      db.query(`SELECT * FROM flight WHERE idflight='${id}'`, (err, result) => {
+        if (err) {
+          reject(new Error(err));
+        } else {
+          resolve(result);
+        }
+      });
+    });
+  },
 
   getId: (id) => {
     return new Promise((resolve, reject) => {
-      db.query(
-        `SELECT idflight,airlines.idairlines,name_airlines,
-      airlines.photo as photoairlines, fromcity.idcity,
-      fromcity.name_city as formcity, fromcountry.idcountry, fromcountry.name_country as fromcountry,
-      tocity.idcity, tocity.name_city as toname_city, tocountry.idcountry,
-      tocountry.name_country as tocountry,code,classflight,typeflight,child,
-      adult,transit,direct,more_transit,luggage,meal,wifi,date_departure,
-      departure,arrived,price,rating,total_review,
-      flight.createdAt FROM (((((flight INNER JOIN airlines ON flight.idairlines=airlines.idairlines)
-      INNER join city as fromcity on flight.idfromcity=fromcity.idcity)
-      INNER JOIN country as fromcountry ON fromcity.idcity=fromcountry.idcountry)
-      INNER JOIN city as tocity on flight.idtocity=tocity.idcity)
-      INNER JOIN country as tocountry on tocity.idcity=tocountry.idcountry) WHERE idflight=${id}`,
-        (err, result) => {
-          // console.log(id);
-          if (err) {
-            reject(new Error(err));
-          } else {
-            resolve(result);
-          }
+      db.query(`
+        SELECT flight.*, a.name_airlines as airlane_name, cf.name_city as from_city,
+        ct.name_city as to_city, f_c.name_country as from_country, t_c.name_country as to_country FROM flight
+        INNER JOIN airlines as a ON flight.idairlines = a.idairlines
+        INNER JOIN city as cf ON flight.idfromcity = cf.idcity
+        INNER JOIN city as ct ON flight.idtocity = ct.idcity
+        INNER JOIN country as f_c ON cf.idcountry = f_c.idcountry
+        INNER JOIN country as t_c ON ct.idcountry = t_c.idcountry
+        WHERE flight.idflight = ${id}
+      `,(err,result) => {
+        if(!err){
+          resolve(result);
+        }else{
+          reject(new Error(err));
         }
-      );
+      })
     });
   },
 
@@ -76,35 +70,32 @@ module.exports = {
     priceto
   ) => {
     return new Promise((resolve, reject) => {
-      db.query(
-        `SELECT idflight,airlines.idairlines,name_airlines,
-      airlines.photo as photoairlines, fromcity.idcity,
-      fromcity.name_city as fromname_city, fromcountry.idcountry, fromcountry.name_country as fromcountry,
-      tocity.idcity, tocity.name_city as toname_city, tocountry.idcountry,
-      tocountry.name_country as tocountry,code,classflight,typeflight,child,
-      adult,transit,direct,more_transit,luggage,meal,wifi,date_departure,
-      departure,arrived,price,rating,total_review,
-      flight.createdAt FROM (((((flight INNER JOIN airlines ON flight.idairlines=airlines.idairlines)
-      INNER join city as fromcity on flight.idfromcity=fromcity.idcity)
-      INNER JOIN country as fromcountry ON fromcity.idcity=fromcountry.idcountry)
-      INNER JOIN city as tocity on flight.idtocity=tocity.idcity)
-      INNER JOIN country as tocountry on tocity.idcity=tocountry.idcountry)
-      WHERE fromcity.name_city LIKE '%${fromcity}%' AND tocity.name_city LIKE '%${tocity}%'
-      AND typeflight LIKE '%${typeflight}%' AND child LIKE '%${child}%' AND adult LIKE '%${adult}%'
-      AND classflight LIKE '%${classflight}%' AND date_departure LIKE '%${datedeparture}%'
-      AND name_airlines LIKE '%${name}%' AND luggage LIKE '%${luggage}%' AND meal LIKE '%${meal}%'
-      AND wifi LIKE '%${wifi}%' AND direct LIKE '%${direct}%' AND transit  LIKE '%${transit}%'
-      AND more_transit  LIKE '%${more_transit}%' AND departure BETWEEN '${departurefrom}'
-      AND '${departureto}' AND arrived BETWEEN '${arrivedfrom}' AND '${arrivedto}'
-      AND price BETWEEN '${pricefrom}' AND '${priceto}'`,
-        (err, result) => {
-          if (err) {
-            reject(new Error(err));
-          } else {
-            resolve(result);
-          }
+      db.query(`
+        SELECT flight.*, a.name_airlines as airlane_name, cf.name_city as from_city,
+        ct.name_city as to_city, f_c.name_country as from_country, t_c.name_country as to_country 
+        FROM flight
+        INNER JOIN airlines as a ON flight.idairlines = a.idairlines
+        INNER JOIN city as cf ON flight.idfromcity = cf.idcity
+        INNER JOIN city as ct ON flight.idtocity = ct.idcity
+        INNER JOIN country as f_c ON cf.idcountry = f_c.idcountry
+        INNER JOIN country as t_c ON ct.idcountry = t_c.idcountry
+        WHERE cf.name_city LIKE '%${fromcity}%' AND ct.name_city LIKE '%${tocity}%'
+        AND flight.typeflight LIKE '%${typeflight}%' AND flight.child LIKE '%${child}%'
+        AND flight.adult LIKE '%${adult}%' AND flight.classflight LIKE '%${classflight}%'
+        AND flight.date_departure LIKE '%${datedeparture}%' AND a.name_airlines LIKE '%${name}%'
+        AND flight.luggage LIKE '%${luggage}%' AND  flight.meal LIKE '%${meal}%'
+        AND flight.wifi LIKE '%${wifi}%' AND flight.direct LIKE '%${direct}%'
+        AND flight.transit LIKE '%${transit}%' AND flight.moretransit LIKE '%${more_transit}%'
+        AND flight.departure BETWEEN '${departurefrom}' AND '${departureto}' 
+        AND flight.arrived BETWEEN '${arrivedfrom}' AND '${arrivedto}'
+        AND flight.price BETWEEN '${pricefrom}' AND '${priceto}'`
+        ,(err,result) => {
+        if(!err){
+          resolve(result);
+        }else{
+          reject(err);
         }
-      );
+      })
     });
   },
 
