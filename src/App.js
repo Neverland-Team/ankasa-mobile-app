29455aad1c0f56edd797d7b3d56cb71090aa6d7a
muhammadridwan47 <rolilaz47@gmail.com>
2020-11-30 10:28:56 +0700

    clear make splash screen

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import React from 'react';
+import React,{useEffect} from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import Router from './router';
 import { Provider } from "react-redux";
 import configureStore from "./redux/store";
 import { PersistGate } from "redux-persist/integration/react";
+import SplashScreen from 'react-native-splash-screen';
 const App = () => {
+   useEffect(() => {
+       SplashScreen.hide();
+   }, []);
   const { store, persistor } = configureStore();
   return (
     <>
