aed498b360b8caecf4c556d3fed985cba375f6de
dyaksa <diasnour0395@gmail.com>
2020-12-01 20:25:29 +0700

    change server https

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const bodyParser = require("body-parser");
 const express = require("express");
 const app = express();
-const server = require("http").createServer(app);
+const server = require("https").createServer(app);
 const io = require("socket.io")(server);
 
 const db = require("./src/Config/db");
