382d0a43ce6303f33b96103056ddcf427b3151d4
muhammadridwan47 <rolilaz47@gmail.com>
2020-12-09 14:49:47 +0700

    Change URI

--- src/components/CardRounded/index.js
+++ src/components/CardRounded/index.js
@@ -5,7 +5,7 @@ export default function CardRounded({country,onPress}) {
     return (
         <TouchableOpacity style={styles.wrapper} onPress={() => onPress()}>
             <View style={styles.container}>
-                <Image source={{uri:'https://i.stack.imgur.com/N4TSy.jpg'}} style={styles.image} />
+                <Image source={{uri:'https://cf.bstatic.com/images/hotel/max500/222/222790006.jpg'}} style={styles.image} />
             </View>
             <Text style={styles.text}>{country}</Text>
         </TouchableOpacity>
--- src/utils/URI/index.js
+++ src/utils/URI/index.js
@@ -1,10 +1,7 @@
-<<<<<<< HEAD
-export const URI = 'http://35.170.200.20:8000/api/v1';
-=======
 export const URI = 'http://192.168.43.31:8000/api/v1';
->>>>>>> bf5fd3e75297a077203b84d9e1c98d600cfe21fb
 export const SOCKETURI = 'http://10.110.235.94:8000';
 export const URIIMAGE = `http://192.168.43.31:8000/`;
+
 
 // export const URI = 'http://54.174.147.209:7000/api/v1';
 // export const SOCKETURI = 'http://54.174.147.209:7000';
