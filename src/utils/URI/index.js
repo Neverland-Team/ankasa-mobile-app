d8b96b2cf83cdb0fe6210e370698d9e1ec0579f3
Muhammad Ridwan <69374541+muhammadridwan47@users.noreply.github.com>
2020-12-24 16:14:38 +0700

    Update index.js

--- src/utils/URI/index.js
+++ src/utils/URI/index.js
@@ -3,8 +3,4 @@
 // export const URIIMAGE = `http://10.110.115.25:8000/public/images/`;
 
 
-export const URI = 'http://ankasa.muhammadriduwan.com/api/v1';
-export const SOCKETURI = 'http://ankasa.muhammadriduwan.com';
-export const URIIMAGE = `http://ankasa.muhammadriduwan.com/public/images/`;
-
 
