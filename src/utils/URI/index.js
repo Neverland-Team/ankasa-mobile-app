7810035807557ae91e8f81829cdbccd54fc511e5
Muhammad Ridwan <69374541+muhammadridwan47@users.noreply.github.com>
2020-12-24 16:14:55 +0700

    Update index.js

--- src/utils/URI/index.js
+++ src/utils/URI/index.js
@@ -1,6 +1,6 @@
-// export const URI = 'http://10.110.115.25:8000/api/v1';
-// export const SOCKETURI = 'http://10.110.235.94:8000';
-// export const URIIMAGE = `http://10.110.115.25:8000/public/images/`;
+ export const URI = 'http://10.110.115.25:8000/api/v1';
+ export const SOCKETURI = 'http://10.110.235.94:8000';
+ export const URIIMAGE = `http://10.110.115.25:8000/public/images/`;
 
 
 
